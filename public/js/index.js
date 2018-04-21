72d51acc42a8abc76deedb4315cf74c41bd87da5
yappynoppy <hyano1991@gmail.com>
2018-04-21 21:21:26 +0900

    search

--- public/js/index.js
+++ public/js/index.js
@@ -78,8 +78,8 @@ var load_html_and_insert = function (html_url, insert_info_arr){
         timeout : 1000,
         datatype: 'html'
     }).then(function(data){
-        var out_html = $.parseHTML(data);//parse1
-        alert(data);
+        var out_html = $(data);//parse1
+        alert(out_html);
         var i;
         for(i = 0; i < insert_info_arr.length; ++i){
             alert(out_html.filter("#" + insert_info_arr[i][0])[0].innerHTML);
