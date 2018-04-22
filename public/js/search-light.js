52d09f9e2ea173dfa13a6bb95fd289f5848b3a91
yappynoppy <hyano1991@gmail.com>
2018-04-22 20:15:03 +0900

    search

---
+++ public/js/search-light.js
@@ -0,0 +1,68 @@
+$(function () {
+
+  var parameter = getParameter();
+  var totalCount = 0;
+
+  if(parameter != ''){
+      load_html_and_insert(["target-area-list", "target-area-list"], parameter); 
+  }
+  
+});
+
+var load_html_and_insert = function (insert_info_arr, parameter){
+
+        var listById_innerHTML = $(document).find("#" + insert_info_arr[1])[0].innerHTML;
+
+        $("#" + insert_info_arr[0]).empty().append(doHighLight(parameter,listById_innerHTML));
+
+};
+
+
+var doHighLight = function(searchStr, str) {
+    if (!searchStr || !str) return 0;
+ 
+    var lowerSearchStr = searchStr.toLowerCase();
+    var lowerStr = str.toLowerCase();
+    var pos = lowerStr.indexOf(lowerSearchStr);
+    var highLitedStr = str;
+    let spanHead = '<span class="highlight">';
+    let spanTail = '</span>';
+    let spanLen = spanHead.length + spanTail.length;
+    let searchStrLen = searchStr.length;
+    
+    while (pos !== -1) {
+
+        highLitedStr = [highLitedStr.slice(0, pos), spanHead , 
+                        highLitedStr.slice(pos, pos + searchStrLen), 
+                        spanTail,highLitedStr.slice(pos + searchStrLen) ].join('');
+
+        pos = highLitedStr.toLowerCase().indexOf(lowerSearchStr, pos + spanLen + 1);    
+    }
+ 
+    return highLitedStr;
+};
+
+
+function enter(code)
+{
+  //エンターキー押下なら
+  if(13 === code)
+  {
+    var para = '?' + document.getElementById('search-text').value;
+    window.location.href = 'searchresult.html' + para; // 通常の遷移
+  }
+}
+
+
+function getParameter(){
+  // URLのパラメータを取得
+  var urlParam = location.search.substring(1);
+ 
+  // URLにパラメータが存在する場合
+  if(urlParam) {
+    return decodeURI(urlParam).trim();
+  }else{
+    return '';
+  }
+ 
+}
