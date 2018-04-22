cdf19ecc59b1c9c4a939e507ae5762127ec4a9d3
yappynoppy <hyano1991@gmail.com>
2018-04-22 13:42:14 +0900

    search

--- public/js/index.js
+++ public/js/index.js
@@ -89,7 +89,7 @@ var load_html_and_insert = function (html_url, insert_info_arr, parameter, count
         alert("dom name:" + listById_dom.childNodes[0].textContent);
 
         var listById = '';
-        if (!listById_dom.hasChildNodes()) {
+        if (listById_dom.hasChildNodes()) {
           getAllChildsTexts(listById_dom.childNodes, function(childTextContent){
             listById = listById + childTextContent;
           });
