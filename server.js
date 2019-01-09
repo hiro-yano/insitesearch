4223eb6b5783e9229055414cec1b7b38b16523af
yappynoppy <hyano1991@gmail.com>
2019-01-10 01:08:50 +0900

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 
-var express = require('express');
-var app = express();
+var express        = require('express');
+var morgan         = require('morgan');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+var app            = express();
 
 var user = process.env.USER;
 var pass = process.env.PASS;
@@ -11,10 +14,11 @@ if (user && pass) {
   app.use(express.basicAuth(user, pass));
 }
 
-app.use(express.logger('dev'));
-app.use(express.compress());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));     // set the static files location /public/img will be /img for users
+app.use(morgan('dev'));                     // log every request to the console
+app.use(bodyParser.urlencoded({ extended: false }))    // parse application/x-www-form-urlencoded
+app.use(bodyParser.json())    // parse application/json
+app.use(methodOverride());                  // simulate DELETE and PUT
 
-app.listen(app.get('port'), function() {
-  console.log('Server listening on port %s', app.get('port'));
-});
+app.listen(8080);   
+console.log('Magic happens on port 8080');          // shoutout to the user
