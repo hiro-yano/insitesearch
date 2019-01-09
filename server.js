d43a23dff0b2eebc063ac7647e75f5211e41b780
yappynoppy <hyano1991@gmail.com>
2019-01-10 01:47:17 +0900

    make it better

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 var express        = require('express');
 var app            = express();
-/*
+
 var user = process.env.USER;
 var pass = process.env.PASS;
 
 if (user && pass) {
   app.use(express.basicAuth(user, pass));
-}*/
+}
 
-app.use(express.logger('dev'));
-app.use(express.compress());
 app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 3000;
