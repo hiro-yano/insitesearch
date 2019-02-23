2fe997a07edf38aae3ba169e79fcaa51eba7c33e
yappynoppy <hyano1991@gmail.com>
2019-02-23 17:25:17 +0900

    server side search

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express        = require('express');
+var fs             = require('fs');
 var app            = express();
+var search = require('./search.js');
 
 var user = process.env.USER;
 var pass = process.env.PASS;
@@ -9,6 +11,27 @@ if (user && pass) {
 }
 
 app.use(express.static(__dirname + '/public'));
+
+app.get("/search/:word", function(req, res) {
+  
+  var totalCount = 0;
+  
+  var pageList = ['index', 'orders', 'products', 'customers', 'reports', 'integrations'];
+  var i;
+
+  for (i = 0; i < pageList.length; ++i) {
+      fs.readFile('./public/src/' + pageList[i] + '.html', 'utf-8' , doReard );
+  }
+
+  function doReard(err, data) {
+    search.create_results(data, req.params.word, );
+  }
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write();
+  res.end();
+
+});
 
 var port = process.env.PORT || 3000;
 app.listen(port);
